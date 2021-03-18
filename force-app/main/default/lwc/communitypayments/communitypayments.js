8f48b7a7b43cc90706302fc3f4b4f425f6a39cec
laurens-findock <laurens@findock.com>
2021-03-18 19:21:13 +0100

    Added css file and some basic input fields

--- force-app/main/default/lwc/communitypayments/communitypayments.js
+++ force-app/main/default/lwc/communitypayments/communitypayments.js
@@ -5,37 +5,43 @@ import submit from '@salesforce/apex/FinDockCommunityController.submit';
 export default class Findockcommunitypayments extends NavigationMixin(LightningElement) {
 
     @track RedirectURL = '';
+    @track ErrorMessage = '';
     handleClick( event ) {
         var input = {
             SuccessURL: 'https://example.com/success',
             FailureURL: 'https://example.com/failure',
             Payer: {
-                Contact : {
+                Contact: {
                     SalesforceFields : {'FirstName':'Bob','LastName':'Smith'}
                 }
             },
             OneTime: {
                 Amount: '10'
             },
-            PaymentMethod : {
-                Name : 'CreditCard',
+            PaymentMethod: {
+                Name: 'CreditCard'
             },
         }
+        // Bundle the form input with additional parameters that the API requires...
+        // ...and trigger API Call to FinDock through the Apex Controller. 
         submit({
             jsonObject: JSON.stringify(input)
-        }).then( data => {
-            console.log(data)
-            this.RedirectURL = data.RedirectURL;
+        // Handle the result of the API call returned by the Controller
+        }).then( result => {
+            console.log(result)
+            // Get the RedirectURL from the result...
+            this.RedirectURL = result.RedirectURL;
+            // ...and redirect to the Hosted Payment Page of the Payment Processor if required.
             this[NavigationMixin.Navigate]({
                 type: 'standard__webPage',
                 attributes: {
                     url: this.RedirectURL
                 }
             });
-
-            // do something with the response
+        // Catch errors
         }).catch ( error => {
-            console.log('Something went horribly wrong!')
+           // Handle errors based on error message
+            console.log('Something went horribly wrong! ' + error)
         });
     }
 }
