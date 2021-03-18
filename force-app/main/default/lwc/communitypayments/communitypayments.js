dd9541eaab157360ca4f6b7f686f012041934025
laurens-findock <laurens@findock.com>
2021-03-18 09:30:50 +0100

    Added controller and basic lwc code

--- force-app/main/default/lwc/communitypayments/communitypayments.js
+++ force-app/main/default/lwc/communitypayments/communitypayments.js
@@ -1,3 +1,31 @@
 import { LightningElement } from 'lwc';
+import submit from '@salesforce/apex/FinDockCommunityController.submit';
 
-export default class Communitypayments extends LightningElement {}
+export default class Findockcommunitypayments extends LightningElement {
+    
+    handleClick( event ) {
+        var input = {
+            SuccessURL: 'https://example.com/success',
+            FailureURL: 'https://example.com/failure',
+            Payer: {
+                Contact : {
+                    SalesforceFields : {'FirstName':'Bob','LastName':'Smith'}
+                }
+            },
+            OneTime: {
+                Amount: '10'
+            },
+            PaymentMethod : {
+                Name : 'CreditCard',
+            },
+        }
+        submit({
+            jsonObject: JSON.stringify(input)
+        }).then( data => {
+            console.log(data)
+            // do something with the response
+        }).catch ( error => {
+            console.log('Something went horribly wrong!')
+        });
+    }
+}
