9e119d88b8c0e17889f4b8188439944bff4c2031
laurens-findock <laurens@findock.com>
2021-03-18 13:56:49 +0100

    Fixes for basic deploy to org

--- force-app/main/default/lwc/communitypayments/communitypayments.js
+++ force-app/main/default/lwc/communitypayments/communitypayments.js
@@ -1,8 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import submit from '@salesforce/apex/FinDockCommunityController.submit';
 
-export default class Findockcommunitypayments extends LightningElement {
-    
+export default class Findockcommunitypayments extends NavigationMixin(LightningElement) {
+
+    @track RedirectURL = '';
     handleClick( event ) {
         var input = {
             SuccessURL: 'https://example.com/success',
@@ -23,6 +25,14 @@ export default class Findockcommunitypayments extends LightningElement {
             jsonObject: JSON.stringify(input)
         }).then( data => {
             console.log(data)
+            this.RedirectURL = data.RedirectURL;
+            this[NavigationMixin.Navigate]({
+                type: 'standard__webPage',
+                attributes: {
+                    url: this.RedirectURL
+                }
+            });
+
             // do something with the response
         }).catch ( error => {
             console.log('Something went horribly wrong!')
